189b98b3f0385893f23cf26007a5917290f6304b
Alvaro Garcia <derecerca@gmail.com>
2018-03-03 16:35:31 +0100

    Refactor: extract class to upper level

--- service/src/main/kotlin/com/example/kata/bank/service/infrastructure/StatementLine.kt
+++ service/src/main/kotlin/com/example/kata/bank/service/infrastructure/StatementLine.kt
@@ -3,7 +3,6 @@ package com.example.kata.bank.service.infrastructure
 import com.example.kata.bank.service.domain.Amount
 import com.example.kata.bank.service.domain.Transaction
 import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 sealed class StatementLine(open val balance: Amount) {
     fun format(format: String): FormattedStatementLine {
@@ -17,47 +16,6 @@ sealed class StatementLine(open val balance: Amount) {
             val values = mapOf("message" to message, "balance" to balance)
             return StatementLineValues(values)
         }
-    }
-
-    class StatementLineValues(val values: Map<String, Any>) {
-        private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/uuuu")
-
-        fun applyTemplate(format: String): FormattedStatementLine {
-            val line = format
-                    .let { consumeMarkers(it) }
-                    .let { removeAllUnusedMarkers(it) }
-            return StatementLine.FormattedStatementLine(line)
-        }
-
-        private fun removeAllUnusedMarkers(format: String) = format.replace("%\\{\\w*}".toRegex(), "")
-
-        private fun consumeMarkers(format: String): String {
-            return values.entries.fold(format, { acc, entry ->
-                acc.replace("%{${entry.key}}", xformat(entry.value))
-            })
-        }
-
-        private fun xformat(value: Any): String {
-            return when (value) {
-                is LocalDateTime -> {
-                    format(value)
-                }
-                is Amount -> {
-                    format(value)
-                }
-                is String -> {
-                    format(value)
-                }
-                else -> {
-                    format(value)
-                }
-            }
-        }
-
-        private fun format(localDateTime: LocalDateTime) = localDateTime.format(this.formatter)
-        private fun format(amount: Amount) = amount.formatted()
-        private fun format(value: String) = value
-        private fun format(any: Any): Nothing = throw IllegalArgumentException("can't format this field: " + any.javaClass)
     }
 
     data class Credit(val date: LocalDateTime, val description: String, val amount: Amount, override val balance: Amount) : StatementLine(balance) {
