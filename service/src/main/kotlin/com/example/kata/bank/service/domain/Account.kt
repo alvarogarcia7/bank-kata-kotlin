6498438558b83535a486c2a3db958578d1afc9e8
Alvaro Garcia <derecerca@gmail.com>
2018-03-03 17:13:08 +0100

    Refactor: split responsibility between creating StatementLine and Statement

--- service/src/main/kotlin/com/example/kata/bank/service/domain/Account.kt
+++ service/src/main/kotlin/com/example/kata/bank/service/domain/Account.kt
@@ -16,7 +16,27 @@ class Account(private val clock: Clock) {
     }
 
     fun printStatement(statementPrinter: StatementPrinter) {
-        statementPrinter.print(Statement.including(StatementLine.initial(), transactionRepository.findAll()))
+        val statementLines = StatementLines.parse(StatementLine.initial(), transactionRepository.findAll())
+        val statement = Statement.inReverseOrder(statementLines)
+        statementPrinter.print(statement)
+    }
+
+    class StatementLines {
+        companion object {
+            fun parse(initialStatement: StatementLine, transactions: List<Transaction>): List<StatementLine> {
+                val initial = Pair(initialStatement, mutableListOf<StatementLine>())
+                val (_, statementLines) = transactions.fold(initial,
+                        { (previousStatement, result), current ->
+                            val currentStatement = StatementLine.parse(current, previousStatement.balance)
+                            result.add(currentStatement)
+                            Pair(currentStatement, result)
+                        })
+
+                return listOf(initialStatement).union(statementLines).toList()
+            }
+        }
+        
+
     }
 
 }
--- service/src/main/kotlin/com/example/kata/bank/service/infrastructure/Statement.kt
+++ service/src/main/kotlin/com/example/kata/bank/service/infrastructure/Statement.kt
@@ -1,23 +1,9 @@
 package com.example.kata.bank.service.infrastructure
 
-import com.example.kata.bank.service.domain.Transaction
-
 class Statement private constructor(val lines: List<StatementLine>) {
     companion object {
-        fun including(
-                initialStatement: StatementLine,
-                transactions: List<Transaction>): Statement {
-
-            val initial = Pair(initialStatement, mutableListOf<StatementLine>())
-            val (_, statementLines) = transactions.reversed().foldRight(initial,
-                    { current, (previousStatement, result) ->
-                        val currentStatement = StatementLine.parse(current, previousStatement.balance)
-                        result.add(currentStatement)
-                        Pair(currentStatement, result)
-                    })
-            statementLines.reverse()
-            statementLines.add(initialStatement)
-            return Statement(statementLines)
+        fun inReverseOrder(statementLines: List<StatementLine>): Statement {
+            return Statement(statementLines.reversed())
         }
     }
 
