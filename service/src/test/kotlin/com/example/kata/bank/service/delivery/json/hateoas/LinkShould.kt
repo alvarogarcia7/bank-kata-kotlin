e507aba6710298dcba2ec1b22a7331395e102b62
Alvaro Garcia <derecerca@gmail.com>
2018-03-09 08:18:35 +0100

    Refactor: prefer using the other style

--- service/src/test/kotlin/com/example/kata/bank/service/delivery/json/hateoas/LinkShould.kt
+++ service/src/test/kotlin/com/example/kata/bank/service/delivery/json/hateoas/LinkShould.kt
@@ -1,20 +1,17 @@
 package com.example.kata.bank.service.delivery.json.hateoas
 
 import com.example.kata.bank.service.domain.Id
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class LinkShould {
     @Test
     fun `generate a self link with a single resource`() {
-        val link = Link.self("accounts" to Id.of("1"))
-        Assertions.assertThat(link.href).isEqualTo("/accounts/1")
-        Assertions.assertThat(link.method).isEqualTo("GET")
-        Assertions.assertThat(link.rel).isEqualTo("self")
+        val id1 = Id.of("1")
+        val linkUsingBuilder = Link.self("accounts" to id1)
+        val manualLink = Link("/accounts/${id1.value}", rel = "self", method = "GET")
 
-        val id = Id.of("1")
-        assertThat(Link("/accounts/${id.value}", rel = "self", method = "GET")).isEqualTo(link)
+        assertThat(manualLink).isEqualTo(linkUsingBuilder)
     }
 
     @Test
