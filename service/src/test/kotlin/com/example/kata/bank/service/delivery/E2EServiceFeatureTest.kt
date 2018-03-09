acbc165f07655dd43f86759f2a8ff9392643ea21
Alvaro Garcia <derecerca@gmail.com>
2018-03-09 07:30:09 +0100

    Refactor: extract this class (#7)

--- service/src/test/kotlin/com/example/kata/bank/service/delivery/E2EServiceFeatureTest.kt
+++ service/src/test/kotlin/com/example/kata/bank/service/delivery/E2EServiceFeatureTest.kt
@@ -22,13 +22,8 @@ import com.example.kata.bank.service.infrastructure.operations.OperationService
 import com.example.kata.bank.service.infrastructure.operations.TimeDTO
 import com.example.kata.bank.service.infrastructure.operations.TransactionDTO
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.core.Request
-import com.github.kittinunf.fuel.core.Response
-import com.github.kittinunf.fuel.httpGet
-import com.github.kittinunf.fuel.httpPost
-import com.github.kittinunf.result.Result
 import org.assertj.core.api.AbstractAssert
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.fail
@@ -334,82 +329,3 @@ class E2EServiceFeatureTest {
  */
 class UnreachableCode : Throwable()
 
-object HTTP {
-
-    private val mapper = JSONMapper.aNew()
-
-    private fun serialize(body: Any): String {
-        return mapper.writeValueAsString(body)
-    }
-
-    fun post(url: String, body: Any): Request {
-        val serializedBody = when (body) {
-            is String -> body
-            else -> serialize(body)
-        }
-        return url.httpPost().header("Content-Type" to "application/json").body(serializedBody, Charsets.UTF_8)
-    }
-
-    fun get(url: String): Request {
-        return url.httpGet()
-    }
-
-    fun request(request: Request): Pair<Response, Result.Success<String, FuelError>> {
-        try {
-            val (_, response, result) = request.responseString()
-            return assertSuccess(response, result)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            fail("exception: " + e.message)
-            throw RuntimeException() // unreachable code
-        }
-    }
-
-    fun <T> assertFailedRequest(
-            request: Request,
-            x: (Response, Result<String, FuelError>) -> T): T {
-        try {
-            val (_, response, result) = request.responseString()
-            return x.invoke(response, result)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            fail("exception: " + e.message)
-            throw RuntimeException() // unreachable code
-        }
-    }
-
-    @Throws(UnreachableCode::class)
-    fun assertSuccess(response: Response, result: Result<String, FuelError>): Pair<Response, Result.Success<String, FuelError>> {
-        return when (result) {
-            is Result.Success -> {
-                val pair = Pair(response, result)
-                pair
-            }
-            is Result.Failure -> {
-                fail("expected a Result.success: " + result.error)
-                throw UnreachableCode()
-            }
-            else -> {
-                fail("expected a Result.success: " + result.javaClass)
-                throw UnreachableCode()
-            }
-        }
-    }
-
-    @Throws(UnreachableCode::class)
-    fun assertError(response: Response, result: Result<String, FuelError>): Pair<Response, Result.Failure<String, FuelError>> {
-        return when (result) {
-            is Result.Success -> {
-                fail("expected a Result.error: " + result.value)
-                throw UnreachableCode()
-            }
-            is Result.Failure -> {
-                Pair(response, result)
-            }
-            else -> {
-                fail("expected a Result.error: " + result.javaClass)
-                throw UnreachableCode()
-            }
-        }
-    }
-}
---
+++ service/src/test/kotlin/com/example/kata/bank/service/delivery/HTTP.kt
@@ -0,0 +1,90 @@
+package com.example.kata.bank.service.delivery
+
+import com.example.kata.bank.service.delivery.json.JSONMapper
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.result.Result
+import org.assertj.core.api.Assertions
+
+object HTTP {
+
+    private val mapper = JSONMapper.aNew()
+
+    private fun serialize(body: Any): String {
+        return mapper.writeValueAsString(body)
+    }
+
+    fun post(url: String, body: Any): Request {
+        val serializedBody = when (body) {
+            is String -> body
+            else -> serialize(body)
+        }
+        return url.httpPost().header("Content-Type" to "application/json").body(serializedBody, Charsets.UTF_8)
+    }
+
+    fun get(url: String): Request {
+        return url.httpGet()
+    }
+
+    fun request(request: Request): Pair<Response, Result.Success<String, FuelError>> {
+        try {
+            val (_, response, result) = request.responseString()
+            return assertSuccess(response, result)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Assertions.fail("exception: " + e.message)
+            throw RuntimeException() // unreachable code
+        }
+    }
+
+    fun <T> assertFailedRequest(
+            request: Request,
+            x: (Response, Result<String, FuelError>) -> T): T {
+        try {
+            val (_, response, result) = request.responseString()
+            return x.invoke(response, result)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Assertions.fail("exception: " + e.message)
+            throw RuntimeException() // unreachable code
+        }
+    }
+
+    @Throws(UnreachableCode::class)
+    fun assertSuccess(response: Response, result: Result<String, FuelError>): Pair<Response, Result.Success<String, FuelError>> {
+        return when (result) {
+            is Result.Success -> {
+                val pair = Pair(response, result)
+                pair
+            }
+            is Result.Failure -> {
+                Assertions.fail("expected a Result.success: " + result.error)
+                throw UnreachableCode()
+            }
+            else -> {
+                Assertions.fail("expected a Result.success: " + result.javaClass)
+                throw UnreachableCode()
+            }
+        }
+    }
+
+    @Throws(UnreachableCode::class)
+    fun assertError(response: Response, result: Result<String, FuelError>): Pair<Response, Result.Failure<String, FuelError>> {
+        return when (result) {
+            is Result.Success -> {
+                Assertions.fail("expected a Result.error: " + result.value)
+                throw UnreachableCode()
+            }
+            is Result.Failure -> {
+                Pair(response, result)
+            }
+            else -> {
+                Assertions.fail("expected a Result.error: " + result.javaClass)
+                throw UnreachableCode()
+            }
+        }
+    }
+}
