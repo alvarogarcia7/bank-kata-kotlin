563487bd578dfbb8ed42b29d94dba0c6731c72ac
Alvaro Garcia <derecerca@gmail.com>
2018-03-09 08:17:43 +0100

    Green: add test for multiple resources

--- service/src/main/kotlin/com/example/kata/bank/service/delivery/json/hateoas/Link.kt
+++ service/src/main/kotlin/com/example/kata/bank/service/delivery/json/hateoas/Link.kt
@@ -4,8 +4,8 @@ import com.example.kata.bank.service.domain.Id
 
 data class Link(val href: String, val rel: String, val method: String) {
     companion object {
-        fun self(vararg resources: Pair<String, Id>): Link {
-            return Link(buildHref(resources), "self", "GET")
+        fun self(first: Pair<String, Id>, vararg resources: Pair<String, Id>): Link {
+            return Link(buildHref(arrayOf(first, *resources)), "self", "GET")
         }
 
         private fun buildHref(resources: Array<out Pair<String, Id>>): String {
--- service/src/test/kotlin/com/example/kata/bank/service/delivery/json/hateoas/LinkShould.kt
+++ service/src/test/kotlin/com/example/kata/bank/service/delivery/json/hateoas/LinkShould.kt
@@ -16,4 +16,20 @@ internal class LinkShould {
         val id = Id.of("1")
         assertThat(Link("/accounts/${id.value}", rel = "self", method = "GET")).isEqualTo(link)
     }
+
+    @Test
+    fun `generate a self link with multiple resources`() {
+        val id1 = Id.of("1")
+        val id2 = Id.of("2")
+        val linkUsingBuilder = Link.self("accounts" to id1, "operations" to id2)
+        val manualLink = Link("/accounts/${id1.value}/operations/${id2.value}", rel = "self", method = "GET")
+
+        assertThat(manualLink).isEqualTo(linkUsingBuilder)
+    }
+
+    @Test
+    fun `can't generate a link without resources`() {
+        // val linkUsingBuilder = Link.self()
+        // this will not compile
+    }
 }
