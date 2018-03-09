2c616fb939b9ec4abbba50f2d660134fb9ae644a
Alvaro Garcia <derecerca@gmail.com>
2018-03-09 08:58:00 +0100

    Green: parse a link from its href

--- service/src/main/kotlin/com/example/kata/bank/service/delivery/json/hateoas/Link.kt
+++ service/src/main/kotlin/com/example/kata/bank/service/delivery/json/hateoas/Link.kt
@@ -1,5 +1,6 @@
 package com.example.kata.bank.service.delivery.json.hateoas
 
+import arrow.core.Either
 import com.example.kata.bank.service.domain.Id
 
 data class Link(val href: String, val rel: String, val method: String) {
@@ -11,6 +12,48 @@ data class Link(val href: String, val rel: String, val method: String) {
         private fun buildHref(resources: Array<out Pair<String, Id>>): String {
             return "/" + resources.map { "${it.first}/${it.second.value}" }
                     .joinToString("/")
+        }
+
+        fun parse(value: String): Either<List<Exception>, Link> {
+            fun inPairs(it: String): Either<List<Exception>, List<Pair<String, String>>> {
+                val values = it.split("/")
+                if (values.size == 1) { // a single chunk
+                    return Either.left(listOf(Exception("no resources")))
+                }
+                var i = 1 // need to skip the first slash
+                val result: MutableList<Pair<String, String>> = mutableListOf()
+                val exceptions = mutableListOf<Exception>()
+                while (i < values.size - 1) {
+                    val resource = values[i]
+                    if (resource.isNullOrBlank()) {
+                        exceptions.add(Exception("no resource"))
+                    }
+                    if (i + 1 < values.size) {
+                        val id = values[i + 1]
+                        i += 2
+                        if (id.isNullOrBlank()) {
+                            exceptions.add(Exception("no resource id"))
+                            break
+                        }
+                        result.add(Pair(resource, id))
+                    } else {
+                        break
+                    }
+                }
+                if (i == 1) {
+                    exceptions.add(Exception("no resource"))
+                }
+                return if (result.isEmpty()) {
+                    Either.left(exceptions)
+                } else {
+                    Either.right(result.toList())
+                }
+            }
+
+            val pairs = inPairs(value)
+                    .map { it -> it.map { (resource, idValue) -> Pair(resource, Id.of(idValue)) } }
+                    .map { pairs -> Link.self(pairs.first(), *pairs.subList(1, pairs.size).toTypedArray()) }
+            return pairs
         }
     }
 
--- service/src/test/kotlin/com/example/kata/bank/service/delivery/json/hateoas/LinkShould.kt
+++ service/src/test/kotlin/com/example/kata/bank/service/delivery/json/hateoas/LinkShould.kt
@@ -1,7 +1,9 @@
 package com.example.kata.bank.service.delivery.json.hateoas
 
+import arrow.core.Either
 import com.example.kata.bank.service.domain.Id
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.SoftAssertions
 import org.junit.jupiter.api.Test
 
 internal class LinkShould {
@@ -28,5 +30,62 @@ internal class LinkShould {
     fun `can't generate a link without resources`() {
         // val linkUsingBuilder = Link.self()
         // this will not compile
+    }
+
+    @Test
+    fun `parse link with a single resource`() {
+        val id1 = Id.of("1")
+        val link = Link.self("accounts" to id1)
+
+        assertThat(Link.parse(link.href)).isEqualTo(Either.right(link))
+    }
+
+    @Test
+    fun `parse link with a single resource - one ending slash`() {
+        val id1 = Id.of("1")
+        val link = Link.self("accounts" to id1)
+
+        assertThat(Link.parse(link.href + "/")).isEqualTo(Either.right(link))
+    }
+
+    @Test
+    fun `parse link with a single resource - multiple ending slashes`() {
+        val id1 = Id.of("1")
+        val link = Link.self("accounts" to id1)
+
+        val softly = SoftAssertions()
+        softly.assertThat(Link.parse(link.href + "//")).isEqualTo(Either.right(link))
+        softly.assertThat(Link.parse(link.href + "///")).isEqualTo(Either.right(link))
+        softly.assertThat(Link.parse(link.href + "////")).isEqualTo(Either.right(link))
+        softly.assertThat(Link.parse(link.href + "/////")).isEqualTo(Either.right(link))
+        softly.assertAll()
+    }
+
+    @Test
+    fun `parse a self link with multiple resources`() {
+        val id1 = Id.of("1")
+        val id2 = Id.of("2")
+        val link = Link.self("accounts" to id1, "operations" to id2)
+
+        assertThat(Link.parse(link.href)).isEqualTo(Either.right(link))
+    }
+
+    @Test
+    fun `can't parse an empty link`() {
+        assertExceptionsMatch(Link.parse(""), "no resources")
+    }
+
+    @Test
+    fun `can't parse a link without resource`() {
+        assertExceptionsMatch(Link.parse("/"), "no resource")
+    }
+
+    @Test
+    fun `can't parse a link without resource or its id`() {
+        assertExceptionsMatch(Link.parse("//"), "no resource", "no resource id")
+    }
+
+    private fun assertExceptionsMatch(actual: Either<List<Exception>, Link>, vararg messages: String) {
+        assertThat(actual.mapLeft { it.map { it.message } }).isEqualTo(Either.left(listOf(* messages)))
     }
 }
