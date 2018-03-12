436ca1f035de14127dee8e135b34609d61b24599
Alvaro Garcia <derecerca@gmail.com>
2018-03-12 08:27:58 +0100

    Refactor: rename

--- service/src/main/kotlin/com/example/kata/bank/service/domain/transactions/Transaction.kt
+++ service/src/main/kotlin/com/example/kata/bank/service/domain/transactions/Transaction.kt
@@ -3,23 +3,23 @@ package com.example.kata.bank.service.domain.transactions
 import java.time.LocalDateTime
 
 sealed class Transaction(open val amount: Amount, open val time: LocalDateTime, open val description: String) {
-    abstract fun subtotal(acc: Amount): Amount
+    abstract fun subtotal(amount: Amount): Amount
 
     data class Deposit(override val amount: Amount, override val time: LocalDateTime, override val description: String) : Transaction(amount, time, description) {
-        override fun subtotal(acc: Amount): Amount {
-            return acc.add(amount)
+        override fun subtotal(amount: Amount): Amount {
+            return amount.add(this.amount)
         }
     }
 
     data class Withdrawal(override val amount: Amount, override val time: LocalDateTime, override val description: String) : Transaction(amount, time, description) {
-        override fun subtotal(acc: Amount): Amount {
-            return acc.subtract(amount)
+        override fun subtotal(amount: Amount): Amount {
+            return amount.subtract(this.amount)
         }
     }
 
     data class Cost(override val amount: Amount, override val time: LocalDateTime, override val description: String) : Transaction(amount, time, description) {
-        override fun subtotal(acc: Amount): Amount {
-            return acc.subtract(amount)
+        override fun subtotal(amount: Amount): Amount {
+            return amount.subtract(this.amount)
         }
     }
 }
