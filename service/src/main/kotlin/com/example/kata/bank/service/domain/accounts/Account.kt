ddff53a9f4cb7b0ee2632cb8ba471a5fe418e2b7
Alvaro Garcia <derecerca@gmail.com>
2018-03-14 07:26:12 +0100

    Refactor: DRY - use existing behaviour

--- service/src/main/kotlin/com/example/kata/bank/service/domain/accounts/Account.kt
+++ service/src/main/kotlin/com/example/kata/bank/service/domain/accounts/Account.kt
@@ -24,8 +24,7 @@ class Account(private val clock: Clock, val name: String, val type: AccountType 
     @Synchronized
     fun withdraw(operationAmount: Amount, description: String): Either<List<Exception>, Id> {
         if (type == AccountType.Personal) {
-            val total = transactionRepository.findAll().foldRight(Amount.of("0"), { ele, acc -> acc.add(ele.value.amount) })
-            if (operationAmount.greaterThan(total)) {
+            if (operationAmount.greaterThan(balance())) {
                 return Either.left(listOf(Exception("Cannot go overdraft")))
             }
         }
