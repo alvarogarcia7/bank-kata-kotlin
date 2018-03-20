ca531f0bb6cf801b31f3534e41451eb4db505e05
Alvaro Garcia <derecerca@gmail.com>
2018-03-20 07:27:05 +0100

    Green: automatically the lambda transitions

--- service/src/test/kotlin/com/example/kata/bank/service/FiniteStateMachineShould.kt
+++ service/src/test/kotlin/com/example/kata/bank/service/FiniteStateMachineShould.kt
@@ -13,6 +13,30 @@ class FiniteStateMachineShould {
 
         Assertions.assertThat(stableState).isEqualTo(newState)
     }
+
+    @Test
+    fun `automatically consume the lambda-transitions`() {
+
+        val stableState = StableState()
+        val state = NonStableState(LambdaTransition({ _: State -> stableState }))
+
+        val newState = state.run()
+
+        Assertions.assertThat(newState).isEqualTo(stableState)
+    }
+}
+
+class NonStableState(private val lambdaTransition: LambdaTransition) : State {
+    override fun run(): State {
+        return lambdaTransition.perform(this)
+    }
+
+}
+
+class LambdaTransition(private val function: (State) -> StableState) {
+    fun perform(previous: State): State {
+        return function.invoke(previous)
+    }
 }
 
 interface State {
