7071682377aff2e142e795b125b4ca4de2495845
Alvaro Garcia <derecerca@gmail.com>
2018-03-20 07:38:18 +0100

    Refactor: make generic, creating space for the upcoming feature

--- service/src/test/kotlin/com/example/kata/bank/service/FiniteStateMachineShould.kt
+++ service/src/test/kotlin/com/example/kata/bank/service/FiniteStateMachineShould.kt
@@ -4,48 +4,54 @@ import org.assertj.core.api.Assertions
 import org.junit.Test
 
 class FiniteStateMachineShould {
+    private val emptyCar = Car(listOf())
+
     @Test
     fun `stay at a stable state`() {
 
-        val stableState = FinalState()
+        val stableState = FinalState(emptyCar)
 
         val newState = stableState.run()
 
         Assertions.assertThat(stableState).isEqualTo(newState)
+        Assertions.assertThat(stableState.payload).isEqualTo(newState.payload)
     }
 
     @Test
     fun `automatically consume the lambda-transitions`() {
 
-        val stableState = FinalState()
-        val state = TransitionState(LambdaTransition({ _: State -> stableState }))
+        val stableState = FinalState(emptyCar)
+        val state = TransitionState(emptyCar, LambdaTransition({ _: State<Car> -> stableState }))
 
         val newState = state.run()
 
         Assertions.assertThat(newState).isEqualTo(stableState)
+        Assertions.assertThat(stableState.payload).isEqualTo(newState.payload)
     }
 }
 
-class TransitionState(private val lambdaTransition: LambdaTransition) : State {
-    override fun run(): State {
+data class Car(val parts: List<String>)
+
+class TransitionState<T>(override val payload: T, private val lambdaTransition: LambdaTransition<T>) : State<T> {
+    override fun run(): State<T> {
         return lambdaTransition.perform(this)
     }
 
 }
 
-class LambdaTransition(private val function: (State) -> State) {
-    fun perform(previous: State): State {
+class LambdaTransition<T>(private val function: (State<T>) -> State<T>) {
+    fun perform(previous: State<T>): State<T> {
         return function.invoke(previous)
     }
 }
 
-interface State {
-    fun run(): State
+interface State<T> {
+    fun run(): State<T>
+    val payload: T
 }
 
-class FinalState : State {
-
-    override fun run(): State {
+class FinalState<T>(override val payload: T) : State<T> {
+    override fun run(): State<T> {
         return this
     }
 
