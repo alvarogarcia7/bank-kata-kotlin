e2c8889e5e59dce1168cfe70e1a705fe3e8e34bd
Alvaro Garcia <derecerca@gmail.com>
2018-03-23 19:48:33 +0100

    Refactor: organize in packages

--- service/src/main/kotlin/com/example/kata/bank/service/delivery/in/DTOs.kt
+++ service/src/main/kotlin/com/example/kata/bank/service/delivery/in/DTOs.kt
@@ -9,3 +9,17 @@ data class StatementRequestDTO(val type: String) {
         return Either.cond(this.type == "statement", { this }, { listOf(Exception("This operation is not supported for now")) })
     }
 }
+
+data class OpenAccountRequestDTO(val name: String?) {
+    fun validate(): Either<List<Exception>, OpenAccountRequestDTO> {
+        var errors = mutableListOf<Exception>()
+        if (name == null || name == "") {
+            errors.add(IllegalArgumentException("empty/blank account name"))
+        }
+        return if (errors.isEmpty()) {
+            Either.right(this)
+        } else {
+            Either.left(errors)
+        }
+    }
+}
