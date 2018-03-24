ac8c8042f47bfddfad2d0107c46e35d96daa44a2
Alvaro Garcia <derecerca@gmail.com>
2018-03-24 13:28:12 +0100

    Refactor: remove dead code

--- service/src/main/kotlin/com/example/kata/bank/service/delivery/in/DTOs.kt
+++ service/src/main/kotlin/com/example/kata/bank/service/delivery/in/DTOs.kt
@@ -2,9 +2,7 @@ package com.example.kata.bank.service.delivery.`in`
 
 import arrow.core.Either
 import com.example.kata.bank.service.domain.AccountRequest
-import com.example.kata.bank.service.domain.transactions.Transaction
 
-data class StatementDTO(val transactions: List<Transaction>)
 data class StatementRequestDTO(val type: String) {
     fun validate(): Either<List<Exception>, AccountRequest.StatementRequest> {
         return Either.cond(this.type == "statement", { AccountRequest.StatementRequest.all() }, { listOf(Exception("This operation is not supported for now")) })
