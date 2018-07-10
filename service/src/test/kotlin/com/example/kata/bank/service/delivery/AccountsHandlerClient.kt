3f051484c9c6cc8bc307e31e29e8b6c74204a6c2
Alvaro Garcia <derecerca@gmail.com>
2018-07-10 22:58:57 +0200

    Refactor: can simplify this string

--- service/src/test/kotlin/com/example/kata/bank/service/delivery/AccountsHandlerClient.kt
+++ service/src/test/kotlin/com/example/kata/bank/service/delivery/AccountsHandlerClient.kt
@@ -13,17 +13,13 @@ object AccountsHandlerClient {
     private fun operation(amount: String, operation: String): String {
         return """
             {
-                "type": """ + quote(operation) + """,
+                "type": "$operation",
                 "amount": {
-                    "value": """ + quote(amount) + """,
+                    "value": "$amount",
                     "currency": "EUR"
                 },
                 "description": "yet another operation"
             }
         """.trimIndent()
-    }
-
-    private fun quote(operation: String): String {
-        return "\"$operation\""
     }
 }
